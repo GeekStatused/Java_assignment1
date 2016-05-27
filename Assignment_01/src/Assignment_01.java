5590f15e94a3a3c3e896189ab822b974e3df4dde
GeekStatused <geekstatused@gmail.com>
2016-05-27 14:52:43 -0400

    Java
    Java

---
+++ Assignment_01/src/Assignment_01.java
@@ -0,0 +1,21 @@
+
+public class Assignment_01 {
+
+	public static void main(String[] args) {
+		
+		//Variable 1 initialized to 3
+		int num1 = 3;
+		
+		//Variable 2 initialized to 23
+		float num2 = 23;
+		
+		//Variable for the sum of both. Type casted to hold an int since we are narrowing.
+		int sum = (int) (num1 + num2); 
+		
+		
+		//Print out the value of sum to the console
+		System.out.println(sum);
+
+	}
+
+}
